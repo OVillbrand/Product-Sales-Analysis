77bc788be1bf565e5611232534f62700ee961a32
OVillbrand <ossianwork@gmail.com>
2026-01-22 11:35:30 +0100

    Added 2nd Business question to Analysis.sql
    Added 2nd Business question to Analysis.sql along with initial commands to answer the question, and syntax fixes

--- Analysis.sql
+++ Analysis.sql
@@ -18,12 +18,10 @@ SELECT
     SUM(od.quantityOrdered) AS totalSold,
     -- Calculating the ratio: Stock divided by Sales
     (p.quantityInStock * 1.0 / SUM(od.quantityOrdered)) AS stockToSalesRatio
-FROM
-    products p
-JOIN
-    orderdetails od ON p.productCode = od.productCode
-GROUP BY
-    p.productCode, p.productName, p.quantityInStock
+FROM products as p
+JOIN orderdetails as od 
+	ON p.productCode = od.productCode
+GROUP BY p.productCode, p.productName, p.quantityInStock
 HAVING
     -- Defines "Low" as stock is less than 20% of total sales
     (p.quantityInStock / SUM(od.quantityOrdered)) < 0.2
@@ -34,7 +32,52 @@ ORDER BY
 SELECT 
     p.productLine,
     SUM(od.quantityOrdered * od.priceEach) AS totalRevenue
-FROM orderDetails od
-JOIN products p ON od.productCode = p.productCode
+FROM orderDetails as od
+JOIN products as p 
+	ON od.productCode = p.productCode
 GROUP BY p.productLine
-ORDER BY totalRevenue DESC;
+ORDER BY 
+	totalRevenue DESC;
+
+/* BUSINESS QUESTION 2: Are we meeting our delivery promises, 
+and where are the geographic bottlenecks?
+Created by: Ossian Villbrand
+*/
+
+-- Sub question 1: What is the average Fulfillment Delay? (Since we don't have delivery dates we have to restrict ourselves to processing time for orders)
+SELECT julianday(orders.shippedDate) - julianday(orders.orderDate) as daysOfProcessing 
+FROM orders 
+
+-- Sub question 2: Which countries have the highest rate of Late Shipments?
+SELECT 
+    cust.country, 
+    SUM(julianday(ord.shippedDate) - julianday(ord.requiredDate)) AS total_delay_days,
+    COUNT(ord.orderNumber) AS number_of_late_orders
+FROM orders AS ord 
+JOIN customers AS cust 
+    ON ord.customerNumber = cust.customerNumber
+WHERE 
+	ord.shippedDate > ord.requiredDate
+GROUP BY cust.country
+ORDER BY 
+	total_delay_days DESC;
+
+-- After the initial search only found one delayed shipment I wanted to verify 
+SELECT COUNT(*)
+FROM orders 
+JOIN customers 
+	ON orders.customerNumber = customers.customerNumber
+WHERE 
+	julianday(orders.shippedDate) > julianday(orders.requiredDate);
+
+-- Sub question 3: Which Office is the most efficient?
+SELECT ofc.city,
+	ROUND(AVG(julianday(ord.shippedDate) - julianday(ord.orderDate)), 1) AS avg_shipping_time
+FROM orders as ord 
+JOIN customers as cust 
+	on ord.customerNumber = cust.customerNumber
+JOIN employees as emp 
+	on emp.employeeNumber = cust.salesRepEmployeeNumber
+JOIN offices as ofc 
+	on ofc.officeCode = emp.officeCode
+GROUP BY ofc.city;
